751ccb566d2dd50739f5c023e848def53de91885
Felipe Cano <fcano@zerofox.com>
2022-02-09 11:21:38 -0300

    Migrations for User model

--- .project_history.sh
+++ .project_history.sh
@@ -1,2 +1,4 @@
  docker-compose run web django-admin startproject profiles_project .
  docker-compose run web python manage.py startapp profiles_api
+ docker-compose run web python manage.py makemigrations profiles_api
+ docker-compose run web python manage.py migrate
